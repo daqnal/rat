c22da098a619250319f69e0c500ab2e35935f5b2
daqnal <dxgd@pm.me>
2025-08-10 00:36:00 -0400

    disable unfinished sidebar for push to main

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <Sidebar />
+      {/* <Sidebar /> */}
       <Background />
       <Scene speed={speed} setSpins={setSpins}/>
       <Counter spins={spins}/>
